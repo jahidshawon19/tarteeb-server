d213b37ed977582268ec554c8daf5e6a695faeca
jahidshawon19 <jahidshawon1730@gmail.com>
2023-08-18 22:28:31 -0700

    vercel config file added

--- index.js
+++ index.js
@@ -69,8 +69,18 @@ async function run() {
       res.send(result)
     })
 
+    
+    // delete booking by id 
+
+    app.delete('/booking/:id', async(req, res)=>{
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
+  
 
 
     // Send a ping to confirm a successful connection
